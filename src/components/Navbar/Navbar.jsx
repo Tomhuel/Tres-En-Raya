8960904918d74abb745fc19d05932e444e4ddca2
= <tomasnahuelantela@gmail.com>
2023-11-15 09:35:14 +0000

    Navbar added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className="navbar">
+                <span className="title">Tres En Raya</span>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
