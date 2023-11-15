c30417f298c182803ca4fff259232f51458ba63b
= <tomasnahuelantela@gmail.com>
2023-11-15 10:23:42 +0000

    Footer added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <h2 className='footer-title'>Tres en raya realizado por <a className='link' href='https://github.com/Tomhuel' target='_blank'>Tomhuel</a></h2>
+        </footer>
+    );
+}
+
+export default Footer;
